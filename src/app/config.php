b4f545093f950902a088961c42a86cfb7df1b6dc
Steve Steele <steven.harville.steele@gmail.com>
2017-03-03 20:19:30 -0600

    Update config organization

--- src/app/config.php
+++ src/app/config.php
@@ -1,11 +1,13 @@
 <?php
 
-// Constants
+// options
 define('ENVIRONMENT', 'local');
+define('AUTO_LATEX', false);
+
+// paths
 define('APP_PATH', 'src/app/');
 define('APP_URL', 'http://shs.job-apps.com:8888/');
 define('AIDE_PATH', 'src/app/interview-aide/');
 define('INTERVIEW_PATH', 'src/app/interview/');
 define('JOBS_PATH', 'src/jobs/');
 define('JOBS_URL', 'http://shs.job-apps.com:8888/src/jobs/');
-define('AUTO_LATEX', false);
