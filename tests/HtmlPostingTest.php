b892b3274fb593b1f559e5988b0e9fd84dc48304
Steve Steele <steven.harville.steele@gmail.com>
2017-03-30 23:26:22 -0500

    Add test for auto-generating a job posting from specified URL

--- tests/HtmlPostingTest.php
+++ tests/HtmlPostingTest.php
@@ -13,7 +13,7 @@ class HtmlPostingTest extends \PHPUnit_Framework_TestCase
 
     public function setup()
     {
-        $this->serverPath = $_SERVER['DOCUMENT_ROOT'];
+        $this->serverPath = $_SERVER['PWD'];
         $this->filePath = JOBS_POSTINGS_PATH;
         $this->fileName = '05-15-2015_junior_devops_engineer_w2o_group';
         $this->postUrl = APP_URL . '/src/jobs/postings/05-05-2015_devops_engineer_w2o_group.php';
@@ -23,8 +23,22 @@ class HtmlPostingTest extends \PHPUnit_Framework_TestCase
 
     public function testConstruct()
     {
-        $this->assertEquals('/' . $this->filePath . '/', $this->htmlPosting->getPath());
+        $this->assertEquals($this->serverPath . '/' . $this->filePath . '/', $this->htmlPosting->getPath());
         $this->assertEquals($this->fileName . '.php', $this->htmlPosting->getFile());
         $this->assertEquals($this->postUrl, $this->htmlPosting->getUrl());
+    }
+
+    public function testGenerate()
+    {
+        $expectedMessage = 'Document generated';
+        $this->assertEquals($expectedMessage, $this->htmlPosting->generate());
+
+        $expectedFile = $this->htmlPosting->getPath() . $this->htmlPosting->getFile();
+        $this->assertFileExists($expectedFile);
+
+        // delete generated file if exists
+        if (file_exists($expectedFile)) {
+            unlink($expectedFile);
+        }
     }
 }
