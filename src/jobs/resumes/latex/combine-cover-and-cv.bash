86d0adb923f9a0bcffb69ed7d9fc28ea7d70b426
Steven Steele <steven.harville.steele@gmail.com>
2018-04-22 22:46:09 -0500

    Add quick command to combine cover letter and cv pdfs together

---
+++ src/jobs/resumes/latex/combine-cover-and-cv.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# mac command
+"/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py" -o ~/Desktop/cv_steve-steele.pdf ../../cover-letters/latex/cover_steve-steele.pdf cv_steve-steele.pdf
+
+exit
