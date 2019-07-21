b6e495a88d5672b3b1e0e9c62c40325c7bb65cfe
Steve Steele <steven.harville.steele@gmail.com>
2019-07-21 13:57:36 -0500

    Update LaTeX libs for manually generated docs

--- src/jobs/cover-letters/latex/generate-manual.bash
+++ src/jobs/cover-letters/latex/generate-manual.bash
@@ -3,11 +3,16 @@
 # set variables
 filename='cover_steve-steele'
 
+# define commands
+latexPath='/Library/TeX/texbin'
+latex="${latexPath}/latex"
+dvipdf="${latexPath}/dvipdfm"
+
 # driver
-latex ${filename}.tex
-latex ${filename}.tex
-latex ${filename}.tex
-dvipdf ${filename}
+${latex} ${filename}.tex >/dev/null 2>&1
+${latex} ${filename}.tex >/dev/null 2>&1
+${latex} ${filename}.tex >/dev/null 2>&1
+${dvipdf} ${filename} >/dev/null 2>&1
 
 echo 'Document generated'
 
