e528fcd8cf29835decc7624c70654b9a2a6bdf18
Steve Steele <steven.harville.steele@gmail.com>
2024-05-15 11:10:52 -0500

    Add SO job board

--- src/jobs/applications/meta.php
+++ src/jobs/applications/meta.php
@@ -14,7 +14,8 @@ $links = [
 ];
 
 $lists = [
-    'Indeed (Laravel)' => 'http://www.indeed.com/jobs?q=%22PHP%22+Laravel+-Java+-.Net+-C+-C%2B%2B+-C%23+-designer&l=Austin%2C+TX&sort=date',
+    'Indeed (Laravel)'       => 'http://www.indeed.com/jobs?q=%22PHP%22+Laravel+-Java+-.Net+-C+-C%2B%2B+-C%23+-designer&l=Austin%2C+TX&sort=date',
+    'Stack Overflow (React)' => 'https://stackoverflow.jobs/search/jobs?searchTerm=React&location=Austin,%20USA',
 ];
 
 $companies = [
